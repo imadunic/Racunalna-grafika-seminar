117ff8bfb1cc4fc9cf36971b217c82e57514f4a8
imadunic <imadun00@fesb.hr>
2022-01-04 17:53:35 +0100

    Camera fixed vertically

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -10,12 +10,12 @@ public class CameraMovement : MonoBehaviour
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        offset = new Vector3(2.75f, 0.76f, -10f);
+        offset = new Vector3(2.75f, 3.5f, -10f);
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = new Vector3(player.position.x + offset.x, Mathf.Clamp(player.position.y + offset.y,-6f,1000f), offset.z);
+        transform.position = new Vector3(player.position.x + offset.x, /*Mathf.Clamp(player.position.y + offset.y,-6f,1000f)*/offset.y, offset.z);
     }
 }

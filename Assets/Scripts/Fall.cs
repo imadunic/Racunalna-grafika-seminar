7b248ac69d0232b067b3b8e34fdacb714d2206c2
imadunic <imadun00@fesb.hr>
2022-01-06 15:39:27 +0100

    Game Over added

--- Assets/Scripts/Fall.cs
+++ Assets/Scripts/Fall.cs
@@ -5,11 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class Fall : MonoBehaviour
 {
+    [SerializeField] private string sceneName;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
         if(collision.gameObject.tag == "Player")
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            SceneManager.LoadScene(sceneName);
         }
     }
 }
